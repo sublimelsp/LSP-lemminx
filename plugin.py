a0ba6e66d9127ea9dbe1c2b197e440e699e17f4b
deathaxe <deathaxe82@gmail.com>
2025-07-25 18:47:11 +0200

    Refactor update check and installation
    1. if "latest" is configured, fetch information about latest available version
       a) from github release for binary server
       b) from maven-metadata.xml on repo.eclipse.org for pure java server
    2. download and directly extract binary server without intermediate files.
    3. remove checksum checks as all the other helper packages also don't do that.
    4. restructure methods to follow LSP-taplo and LSP-PowerShellEditorServices.

--- plugin.py
+++ plugin.py
@@ -1,5 +1,5 @@
 from __future__ import annotations
-import hashlib
+import contextlib
 import json
 import os
 import re
@@ -8,7 +8,8 @@ import time
 import zipfile
 
 from http.client import HTTPException
-from urllib.request import urlretrieve, urlopen
+from io import BytesIO
+from urllib.request import urlretrieve, urlopen, Request as HttpRequest
 
 from LSP.plugin import (
     AbstractPlugin,
@@ -24,56 +25,48 @@ __all__ = ["LemminxPlugin", "plugin_loaded", "plugin_unloaded"]
 
 
 class BaseServerHandler:
-    dest_checksum: str = ""
-    dest_version: str = ""
+    server_version: str = ""
 
     @classmethod
-    def needs_update_or_installation(cls) -> bool:
-        cls.dest_version = str(LemminxPlugin.settings.get("server_version", "latest"))
+    def needs_update_or_installation(cls):
+        server_file = cls.server_binary()
+        is_upgrade = os.path.isfile(server_file)
+        if is_upgrade:
+            next_update_check, server_version = cls.load_metadata()
+        else:
+            next_update_check, server_version = 0, ""
 
-        next_run, version, checksum = cls.load_metadata()
-        is_upgrade = os.path.isfile(cls.server_binary())
+        cls.server_version = str(LemminxPlugin.settings.get("server_version", "latest"))
+        if cls.server_version == "latest":
+            if int(time.time()) >= next_update_check:
+                try:
+                    available_version = cls.available_version()
+                    if available_version != server_version:
+                        cls.server_version = available_version
+                        return True
+                except BaseException:
+                    cls.save_metadata(False, server_version)
 
-        if (
-            is_upgrade
-            and cls.dest_version == version
-            and (cls.dest_version != "latest" or int(time.time()) < next_run)
-        ):
             return False
 
-        try:
-            cls.dest_checksum = cls.download_checksum()
-        except (HTTPException, OSError, ValueError) as e:
-            # downloading or parsing checksum failed
-            if is_upgrade:
-                cls.save_metadata(False, version, checksum)
-                print("LSP-lemminx: Update check failed - " + str(e))
-                return False
-            raise
-
-        if cls.dest_checksum == checksum:
-            # installed binary up to date
-            cls.save_metadata(True, version, checksum)
-            return False
-
-        return True
+        return cls.server_version != server_version
 
     @classmethod
-    def download_checksum(cls) -> str:
+    def available_version(cls) -> str:
         """
-        Build and return platform specific download url.
+        Fetch and return available version to upgrade to.
         """
         raise NotImplementedError()
 
-    @staticmethod
-    def server_binary() -> str:
+    @classmethod
+    def server_binary(cls) -> str:
         """
         Build and return absolute path to installed language server binary.
         """
         raise NotImplementedError()
 
-    @staticmethod
-    def metadata_file() -> str:
+    @classmethod
+    def metadata_file(cls) -> str:
         """
         Build and return absolute path to meta data file
         storing language server's version and checksum.
@@ -81,23 +74,22 @@ class BaseServerHandler:
         raise NotImplementedError()
 
     @classmethod
-    def load_metadata(cls) -> tuple[int, str, str]:
+    def load_metadata(cls) -> tuple[int, str]:
         try:
             with open(cls.metadata_file()) as fobj:
                 data = json.load(fobj)
-                return (int(data["timestamp"]), data["version"], data["checksum"])
+                return int(data["timestamp"]), data["version"]
         except (FileNotFoundError, KeyError, TypeError, ValueError):
-            return (0, "", "")
+            return 0, ""
 
     @classmethod
-    def save_metadata(cls, success: bool, version: str, checksum: str) -> None:
+    def save_metadata(cls, success: bool, version: str) -> None:
         next_run_delay = (7 * 24 * 60 * 60) if success else (6 * 60 * 60)
         with open(cls.metadata_file(), "w") as fobj:
             json.dump(
                 {
                     "timestamp": int(time.time()) + next_run_delay,
                     "version": version,
-                    "checksum": checksum,
                 },
                 fp=fobj,
             )
@@ -130,28 +122,21 @@ class BinaryServerHandler(BaseServerHandler):
 
     @classmethod
     def install_or_update(cls) -> None:
-        if not cls.dest_checksum or not cls.dest_version:
+        if not cls.server_version:
             raise RuntimeError()
 
         server_binary = cls.server_binary()
         server_path, server_name = os.path.split(server_binary)
 
         # downlad and unzip server binary (ignore any other files)
-        path, _ = urlretrieve(cls.make_url(cls.dest_version, "zip"))
-        with zipfile.ZipFile(path) as zf:
-            zf.extract(server_name, server_path)
-        os.remove(path)
+        with contextlib.closing(urlopen(cls.download_url())) as response:
+            with zipfile.ZipFile(BytesIO(response.read())) as arc:
+                arc.extractall(server_path)
 
-        # validate binary's checksum
-        checksum = ""
-        with open(server_binary, "rb") as fobj:
-            checksum = hashlib.sha256(fobj.read()).hexdigest().lower()
-        if checksum != cls.dest_checksum:
-            os.remove(server_binary)
-            raise ValueError("Validating server binary failed!")
+        os.chmod(server_binary, 0o755)
 
         # write update cookie
-        cls.save_metadata(True, cls.dest_version, cls.dest_checksum)
+        cls.save_metadata(True, cls.server_version)
 
     @classmethod
     def can_start(
@@ -169,41 +154,53 @@ class BinaryServerHandler(BaseServerHandler):
     # server specific methods
 
     @classmethod
-    def download_checksum(cls) -> str:
-        # download checksum file
-        response = (
-            urlopen(cls.make_url(cls.dest_version, "sha256"), timeout=2.0).read().decode("utf-8")
-        )
+    def available_version(cls):
+        # response url ends with latest available version number
+        request = HttpRequest(url=f"{cls.repo_url()}/releases/latest", method="HEAD")
+        with contextlib.closing(urlopen(request)) as response:
+            return response.url.rstrip("/").rsplit("/", 1)[1]
 
-        # parse checksum file
-        match = re.search(r"^([a-f0-9]{64})\b", response, re.IGNORECASE)
-        if not match:
-            raise ValueError("Failed to parse checksum file!")
+    @classmethod
+    def repo_url(cls) -> str:
+        return "https://github.com/redhat-developer/vscode-xml"
 
-        return match.group(1).lower()
-
-    @staticmethod
-    def make_url(version: str, ext: str) -> str:
-        name = {
-            "linux": "lemminx-linux",
-            "osx": "lemminx-osx-x86_64",
-            "windows": "lemminx-win32",
+    @classmethod
+    def download_url(cls) -> str:
+        release_assets = {
+            "linux-x64": "lemminx-linux.zip",
+            "osx-arm64": "lemminx-osx-aarch_64.zip",
+            "osx-x64": "lemminx-osx-x86_64.zip",
+            "windows-x64": "lemminx-win32.zip",
         }
-        pattern = "https://github.com/redhat-developer/vscode-xml/releases/download/{0}/{1}.{2}"
-        return pattern.format(version, name[sublime.platform()], ext)
+        try:
+            asset = release_assets[f"{sublime.platform()}-{sublime.arch()}"]
+            return f"{cls.repo_url()}/releases/download/{cls.server_version}/{asset}"
+        except KeyError:
+            raise RuntimeError("Binary server not supported on this platform!")
 
-    @staticmethod
-    def metadata_file() -> str:
+    @classmethod
+    def metadata_file(cls) -> str:
         return os.path.join(LemminxPlugin.server_path(), "binary_server.json")
 
-    @staticmethod
-    def server_binary() -> str:
-        name = {
-            "linux": "lemminx-linux",
-            "osx": "lemminx-osx-x86_64",
-            "windows": "lemminx-win32.exe",
+    @classmethod
+    def server_binary(cls) -> str:
+        names = {
+            "linux-x64": "lemminx-linux",
+            "osx-arm64": "lemminx-osx-aarch_64",
+            "osx-x64": "lemminx-osx-x86_64",
+            "windows-x64": "lemminx-win32.exe",
         }
-        return os.path.join(LemminxPlugin.server_path(), name[sublime.platform()])
+        try:
+            name = names[f"{sublime.platform()}-{sublime.arch()}"]
+            return os.path.join(LemminxPlugin.server_path(), name)
+        except KeyError:
+            raise RuntimeError("Binary server not supported on this platform!")
+
+    @classmethod
+    def is_supported(cls):
+        arch = sublime.arch()
+        os = sublime.platform()
+        return arch == "x64" or os == "osx" and arch == "arm64"
 
 
 class JavaServerHandler(BaseServerHandler):
@@ -221,33 +218,22 @@ class JavaServerHandler(BaseServerHandler):
 
     1. compare `server_version` setting value with version from java_server.json
        -> exit if equal
-    2. download org.eclipse.lemminx-<semver>-uber.sha1
-    3. compare checksum with value from java_server.json
-       -> exit if equal
-    4. download org.eclipse.lemminx-<semver>-uber.jar
-    5. validate checksum of downloaded jar file
+    2. if `server_version` is "latest"...
+       a) download maven-metadata.xml
+       b) read latest version from <release> node
+    3. download org.eclipse.lemminx-<semver>-uber.jar
     """
 
     # API methods
 
     @classmethod
     def install_or_update(cls) -> None:
-        if not cls.dest_checksum or not cls.dest_version:
+        if not cls.server_version:
             raise RuntimeError()
 
-        server_binary = cls.server_binary()
-
-        urlretrieve(cls.make_url(cls.dest_version, "jar"), server_binary)
-
-        checksum = ""
-        with open(server_binary, "rb") as fobj:
-            checksum = hashlib.sha1(fobj.read()).hexdigest().lower()
-        if checksum != cls.dest_checksum:
-            os.remove(server_binary)
-            raise ValueError("Validating server binary failed!")
-
+        urlretrieve(cls.download_url(), cls.server_binary())
         # write update cookie
-        cls.save_metadata(True, cls.dest_version, cls.dest_checksum)
+        cls.save_metadata(True, cls.server_version)
 
     @classmethod
     def can_start(
@@ -265,43 +251,33 @@ class JavaServerHandler(BaseServerHandler):
     # server specific methods
 
     @classmethod
-    def download_checksum(cls) -> str:
-        # download checksum file
-        response = (
-            urlopen(cls.make_url(cls.dest_version, "jar.sha1"), timeout=2.0)
-            .read()
-            .decode("utf-8")
+    def available_version(cls):
+        with contextlib.closing(urlopen(f"{cls.repo_url()}/maven-metadata.xml")) as response:
+            content = response.read().decode("utf-8")
+
+        match = re.search(
+            r"<release>\s*(\d+\.\d+\.\d+)\s*</release>",
+            content,
+            re.IGNORECASE | re.MULTILINE,
         )
-
-        # parse checksum file
-        match = re.search(r"^([a-f0-9]{40})\b", response, re.IGNORECASE)
         if not match:
-            raise ValueError("Failed to parse checksum file!")
+            raise ValueError("Can't determine latest release!")
+        return match.group(1)
 
-        return match.group(1).lower()
+    @classmethod
+    def repo_url(cls) -> str:
+        return "https://repo.eclipse.org/content/repositories/lemminx-releases/org/eclipse/lemminx/org.eclipse.lemminx"
 
-    @staticmethod
-    def make_url(version: str, ext: str) -> str:
-        base_url = "https://repo.eclipse.org/content/repositories/lemminx-releases/org/eclipse/lemminx/org.eclipse.lemminx"
+    @classmethod
+    def download_url(cls) -> str:
+        return f"{cls.repo_url()}/{cls.server_version}/org.eclipse.lemminx-{cls.server_version}-uber.jar"
 
-        if version == "latest":
-            match = re.search(
-                r"<release>\s*(\d+\.\d+\.\d+)\s*</release>",
-                urlopen(base_url + "/maven-metadata.xml").read().decode("utf-8"),
-                re.IGNORECASE | re.MULTILINE,
-            )
-            if not match:
-                raise ValueError("Can't determine latest release!")
-            version = match.group(1)
-
-        return "{0}/{1}/org.eclipse.lemminx-{1}-uber.{2}".format(base_url, version, ext)
-
-    @staticmethod
-    def metadata_file() -> str:
+    @classmethod
+    def metadata_file(cls) -> str:
         return os.path.join(LemminxPlugin.server_path(), "java_server.json")
 
-    @staticmethod
-    def server_binary() -> str:
+    @classmethod
+    def server_binary(cls) -> str:
         return os.path.join(LemminxPlugin.server_path(), "lemminx.jar")
 
 
@@ -394,11 +370,7 @@ class LemminxPlugin(AbstractPlugin):
     @classmethod
     def server(cls) -> type[BinaryServerHandler] | type[JavaServerHandler]:
         if cls._server is None:
-            if (
-                cls.settings.get("server_binary", True)
-                and sublime.platform() in ("linux", "osx", "windows")
-                and sublime.arch() == "x64"
-            ):
+            if cls.settings.get("server_binary", True) and BinaryServerHandler.is_supported():
                 cls._server = BinaryServerHandler
             else:
                 cls._server = JavaServerHandler
